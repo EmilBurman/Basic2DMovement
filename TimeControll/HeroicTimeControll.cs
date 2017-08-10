008e4a8c3588f785019fd2efd86edbd78a5dcb63
Emil Burman <EmilBurman@users.noreply.github.com>
2017-08-10 11:25:38 +0200

    Added FSM
    Added FSM to introduce a cooldown aspect to reversing.

--- TimeControll/HeroicTimeControll.cs
+++ TimeControll/HeroicTimeControll.cs
@@ -31,9 +31,14 @@ public class HeroicTimeControll : MonoBehaviour, ITimeControll
     public GameObject entity;
     private ArrayList keyframes;
 
-    //
+    //Checks for if player is reversing
     private bool isReversing = false;
     private bool firstRun = true;
+    
+    // Cooldown and state variables
+    private TimeState timeState;
+    float reverseTimer; 			// Shows the current cooldown.
+    float reverseCooldownLimit = 5f;        	// Sets the cooldown of the dash in seconds.
 
     //Determine how much to save
     private int keyframe = 5;
@@ -45,9 +50,9 @@ public class HeroicTimeControll : MonoBehaviour, ITimeControll
     private int reverseCounter = 0;
 
     // Variables to interpolate between keyframes
-
     private Vector2 currentPosition;
     private Vector2 previousPosition;
+
 
     void Start()
     {
@@ -62,12 +67,12 @@ public class HeroicTimeControll : MonoBehaviour, ITimeControll
                 frameCounter += 1;
             else
             {
-                frameCounter = 0;
+		frameCounter = 0;
                 keyframes.Add(new Keyframe(entity.transform.position/*, entity.transform.velocity*/));
             }
         }
         else
-            StartCoroutine(ReverseEntityTimeFlow());
+            ReverseAbility();
 
         if (keyframes.Count > 15)
             keyframes.RemoveAt(0);
@@ -86,10 +91,41 @@ public class HeroicTimeControll : MonoBehaviour, ITimeControll
         }
     }
     
+    private void ReverseAbility()
+    {
+	switch (timeState)
+        {
+            case TimeState.Ready:
+                if (dash)
+                {
+                    StartCoroutine(ReverseEntityTimeFlow());
+                    timeState = TimeState.Reversing;
+                }
+                break;
+            case TimeState.Reversing:
+                // Set the cooldown and initate cooldown state.
+                reverseTimer += Time.deltaTime * 3;
+                if (reverseTimer >= reverseCooldownLimit)
+                {
+                    reverseTimer = reverseCooldownLimit;
+                    timeState = TimeState.Cooldown;
+                }
+                break;
+            case TimeState.Cooldown:
+                reverseTimer -= Time.deltaTime;
+                if (reverseTimer <= 0)
+                {
+                    reverseTimer  = 0;
+                    timeState = TimeState.Ready;
+                }
+                break;
+        }
+    }
+    
     IEnumerator ReverseEntityTimeFlow()
     {
-	    if (reverseCounter > 0)
-                reverseCounter -= 1;
+	if (reverseCounter > 0)
+        	reverseCounter -= 1;
         else
             {
                 reverseCounter = keyframe;
@@ -107,6 +143,14 @@ public class HeroicTimeControll : MonoBehaviour, ITimeControll
         }
     }
 }
+
+public enum TimeState
+{
+    Ready,
+    Reversing,
+    Cooldown
+}
+
 public class Keyframe
 {
     public Vector2 position;
