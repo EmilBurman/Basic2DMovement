a7e05e6bf73c33212120ba9e815c446249c684c0
Emil Burman <emil.burman@hotmail.com>
2017-08-14 20:21:18 +0200

    Added crawler, improve raycasting
    Made raycasting more dynamic
    Added simple "crawler" AI

---
+++ Controller/CrawlerController2D.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrawlerController2D : MonoBehaviour, IController2D
+{
+    //Interface----------------------------
+    public bool Dash()
+    {
+        return false;
+    }
+
+    public bool FlashReverse()
+    {
+        return false;
+    }
+
+    public bool Jump()
+    {
+        return false;
+    }
+
+    public float Move()
+    {
+        return direction;
+    }
+
+    public bool SlowReverse()
+    {
+        return false;
+    }
+
+    public bool Sprint()
+    {
+        return false;
+    }
+    //End interface-------------------------
+
+    private ITerrainState stateMachine;
+    public CrawlerState crawlState;                     // Shows the current state of dashing.
+    float direction;
+    // Use this for initialization
+    void Start()
+    {
+        stateMachine = GetComponent<ITerrainState>();
+    }
+    private void Update()
+    {
+        CrawlStateCheck();
+    }
+
+    private void CrawlStateCheck()
+    {
+        switch (crawlState)
+        {
+            case CrawlerState.CrawlRight:
+                if (!stateMachine.WallRight() && stateMachine.Grounded())
+                {
+                    direction = 1;
+                }
+                else
+                    crawlState = CrawlerState.CrawlLeft;
+                break;
+            case CrawlerState.CrawlLeft:
+                if (!stateMachine.WallLeft() && stateMachine.Grounded())
+                {
+                    direction = -1;
+                }
+                else
+                    crawlState = CrawlerState.CrawlRight;
+                break;
+        }
+    }
+}
+
+public enum CrawlerState
+{
+    CrawlRight,
+    CrawlLeft,
+    Stop
+}
---
+++ TerrainStates/HeroicTerrainState.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class HeroicTerrainState : MonoBehaviour, ITerrainState
+{
+    //Interface--------------------
+    public bool Grounded()
+    {
+        return IsGrounded();
+    }
+
+    public bool Airborne()
+    {
+        return (!IsGrounded());
+    }
+
+    public bool WallLeft()
+    {
+        return CheckWallLeft();
+    }
+
+    public bool WallRight()
+    {
+        return CheckWallRight();
+    }
+    //End interface---------------
+
+    // Layermasks
+    public LayerMask groundLayer;                                   // Layermask variable to check for ground layer.
+    public LayerMask wallLayer;                                     // Layermask variable to check for wall layer.
+
+    //Checks for ground.
+    public float groundCheckSpacing = 0.94f;                        // Determines how to space out the left and right ground checks.
+    public float yAxisRayCastStartPoint = 0.01f;                    // Defines how far out from the collider to check for the ground.
+    float yAxisExtendFromCollider;                                  // Gets collider and sets raycast starting point for the ground.
+
+    //Checks for walls.
+    public float wallCheckSpacing = 2f;                             // Determines how to space out the top and bottom wall checks.
+    public float xAxisRayCastStartPoint = 0.01f;                    // Defines how far out from the collider to check for the walls.
+    float xAxisExtendFromCollider;                                  // Gets collider and sets raycast starting point for the ground.
+
+    // Internal system variables.
+    Rigidbody2D rigidbody2D;                                        // Reference to the player's rigidbody.
+    SpriteRenderer mySpriteRenderer;                                // Entity sprite sprite
+
+    // Distance to check
+    public float groundDistanceCheck;                               // Defines the distance to check for terrain.
+    public float wallDistanceCheck;                                 // Defines the distance to check for terrain.
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        yAxisExtendFromCollider = (GetComponent<Collider2D>().bounds.extents.y + yAxisRayCastStartPoint);
+        xAxisExtendFromCollider = (GetComponent<Collider2D>().bounds.extents.x + xAxisRayCastStartPoint);
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        groundDistanceCheck = 0.03f;
+        wallDistanceCheck = 0.03f;
+    }
+
+    bool IsGrounded()
+    {
+        Color color;
+        bool groundCenterCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x, rigidbody2D.transform.position.y - yAxisExtendFromCollider), Vector2.down, groundDistanceCheck, groundLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x, rigidbody2D.transform.position.y - yAxisExtendFromCollider), Vector2.down, color = Color.yellow, 0.1f);
+
+        bool groundRightCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x + groundCheckSpacing, rigidbody2D.transform.position.y - yAxisExtendFromCollider), Vector2.down, groundDistanceCheck, groundLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x + groundCheckSpacing, rigidbody2D.transform.position.y - yAxisExtendFromCollider), Vector2.down, color = Color.green, 0.1f);
+
+        bool groundLeftCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x - groundCheckSpacing, rigidbody2D.transform.position.y - yAxisExtendFromCollider), Vector2.down, groundDistanceCheck, groundLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x - groundCheckSpacing, rigidbody2D.transform.position.y - yAxisExtendFromCollider), Vector2.down, color = Color.red, 0.1f);
+
+        return (groundLeftCheck || groundCenterCheck || groundRightCheck) ? true : false;
+    }
+    bool CheckWallLeft()
+    {
+        Color color;
+        bool leftCenterCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x - xAxisExtendFromCollider, rigidbody2D.transform.position.y), Vector2.left, wallDistanceCheck, wallLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x - xAxisExtendFromCollider, rigidbody2D.transform.position.y), Vector2.left, color = Color.yellow, 0.1f);
+
+        bool leftBottomCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x - xAxisExtendFromCollider, rigidbody2D.transform.position.y - wallCheckSpacing), Vector2.left, wallDistanceCheck, wallLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x - xAxisExtendFromCollider, rigidbody2D.transform.position.y - wallCheckSpacing), Vector2.left, color = Color.green, 0.1f);
+
+        bool leftTopCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x - xAxisExtendFromCollider, rigidbody2D.transform.position.y + wallCheckSpacing), Vector2.left, wallDistanceCheck, wallLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x - xAxisExtendFromCollider, rigidbody2D.transform.position.y + wallCheckSpacing), Vector2.left, color = Color.red, 0.1f);
+
+        return (leftCenterCheck || leftBottomCheck || leftTopCheck) ? true : false;
+    }
+    bool CheckWallRight()
+    {
+        Color color;
+        bool rightCenterCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x + xAxisExtendFromCollider, rigidbody2D.transform.position.y), Vector2.right, wallDistanceCheck, wallLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x + xAxisExtendFromCollider, rigidbody2D.transform.position.y), Vector2.right, color = Color.yellow, 0.1f);
+
+        bool rightBottomCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x + xAxisExtendFromCollider, rigidbody2D.transform.position.y - wallCheckSpacing), Vector2.right, wallDistanceCheck, wallLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x + xAxisExtendFromCollider, rigidbody2D.transform.position.y - wallCheckSpacing), Vector2.right, color = Color.green, 0.1f);
+
+        bool rightTopCheck = Physics2D.Raycast(new Vector2(rigidbody2D.transform.position.x + xAxisExtendFromCollider, rigidbody2D.transform.position.y + wallCheckSpacing), Vector2.right, wallDistanceCheck, wallLayer);
+        Debug.DrawRay(new Vector2(rigidbody2D.transform.position.x + xAxisExtendFromCollider, rigidbody2D.transform.position.y + wallCheckSpacing), Vector2.right, color = Color.red, 0.1f);
+
+        return (rightCenterCheck || rightBottomCheck || rightTopCheck) ? true : false;
+    }
+}
