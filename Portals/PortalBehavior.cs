2584a1e6b2265b35cd0ddd80d0450fba97ea23ff
Emil Burman <emil.burman@hotmail.com>
2017-08-31 22:37:26 +0200

    Polished portals

---
+++ Portals/PortalBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalBehavior : MonoBehaviour
+{
+    //Public variables
+    public GameObject portal;
+    public bool portalToPortal;
+    public float  offSetDistance;
+    public Vector3 exitPoint;
+
+    //Internal variables
+    Angle angleDirection;
+    Vector3 entityOffset;
+    Vector3 offSetDistanceFixed;
+
+    void Start()
+    {
+        if (transform.rotation.eulerAngles.z >= 0 && transform.rotation.eulerAngles.z < 90)
+            angleDirection = Angle.up;
+
+        if (transform.rotation.eulerAngles.z >= 90 && transform.rotation.eulerAngles.z < 180)
+            angleDirection = Angle.left;
+
+        if (transform.rotation.eulerAngles.z >= 180 && transform.rotation.eulerAngles.z < 270)
+            angleDirection = Angle.down;
+
+        if (transform.rotation.eulerAngles.z >= 270 && transform.rotation.eulerAngles.z < 360)
+            angleDirection = Angle.right;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (portalToPortal)
+            Debug.DrawLine(transform.position, portal.transform.position, Color.green);
+        else
+            Debug.DrawLine(transform.position, exitPoint, Color.green);
+    }
+
+    public Vector3 portalExitPoint(Bounds entitySize)
+    {
+        switch (angleDirection)
+        {
+            case Angle.up:
+                entityOffset = new Vector3(0, entitySize.extents.y, 0);
+                offSetDistanceFixed = new Vector3(0, offSetDistance, 0);
+                break;
+            case Angle.right:
+                entityOffset = new Vector3(entitySize.extents.x, 0, 0);
+                offSetDistanceFixed = new Vector3(offSetDistance, 0, 0);
+                break;
+            case Angle.down:
+                entityOffset = new Vector3(0, -entitySize.extents.y, 0);
+                offSetDistanceFixed = new Vector3(0, -offSetDistance, 0);
+                break;
+            case Angle.left:
+                entityOffset = new Vector3(-entitySize.extents.x, 0, 0);
+                offSetDistanceFixed = new Vector3(-offSetDistance, 0, 0);
+                break;
+        }
+        return transform.position + entityOffset + offSetDistanceFixed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (portalToPortal)
+            collision.transform.position = portal.GetComponent<PortalBehavior>().portalExitPoint(collision.bounds);
+        else
+            collision.transform.position = exitPoint;
+    }
+}
+public enum Angle { up, down, right, left }
