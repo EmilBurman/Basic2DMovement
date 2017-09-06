1bec3b672d7f2d134f16f845ae6664abc30aeddf
Emil Burman <EmilBurman@users.noreply.github.com>
2017-09-06 12:56:24 +0200

    Made to take an array of objects

--- GeneralBehaviorScripts/PreassurePad.cs
+++ GeneralBehaviorScripts/PreassurePad.cs
@@ -4,35 +4,34 @@ using UnityEngine;
 
 public class PreassurePad : MonoBehaviour
 {
-    // Which door to interact with
-    public GameObject door;
-    IDoor doorControllScript;
+     // Which doors to interact with
+    public GameObject[] door;
 
     //Public variables
     public float massTriggerAmount;
 
-    void Start()
-    {
-        doorControllScript = door.GetComponent<IDoor>();
-    }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (doorControllScript.isClosed() && collision.GetComponent<Rigidbody2D>().mass > massTriggerAmount)
-        {
+        for(int i:int=0; i<door.Length; i++)
+   	    {
+            if (door[i].GetComponent<IDoor>().isClosed() && collision.GetComponent<Rigidbody2D>().mass > massTriggerAmount)
+            {
             Debug.Log("Opening door");
-            doorControllScript.Open();
-        }
-
-        else if (doorControllScript.isOpen() && collision.GetComponent<Rigidbody2D>().mass > massTriggerAmount)
-        {
+       	    door[i].GetComponent<IDoor>().Open();
+            }
+            else if (door[i].GetComponent<IDoor>().isOpen() && collision.GetComponent<Rigidbody2D>().mass > massTriggerAmount)
+            {
             Debug.Log("Closing door");
-            doorControllScript.Close();
+       	    door[i].GetComponent<IDoor>().Close();
+   	        }
         }
     }
 
     void Update()
     {
-        Debug.DrawLine(transform.position, door.transform.position, Color.green);
-    }
+        for(int i:int=0; i<door.Length; i++)
+   	    {
+        Debug.DrawLine(transform.position, door[i].transform.position, Color.green);
+    	}
+   }
 }
